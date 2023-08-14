366ce570e047b7bff9468c91d19915854de29372
dsm5e <justry24@gmail.com>
2023-08-14 20:39:48 +0300

    Initial Commit

---
+++ Spar/SparApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparApp.swift
+//  Spar
+//
+//  Created by dsm 5e on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
